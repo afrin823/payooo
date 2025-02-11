f9d9c7db8d3399c541ced71f878ddd20b83d0008
afrin823 <afrin12490@gmail.com>
2025-02-11 11:28:20 +0600

    paymnet

--- js/home.js
+++ js/home.js
@@ -3,5 +3,16 @@ document.getElementById("add-money-btn")
     event.preventDefault();
     const Amount = document.getElementById("amount").value;
     const pinNumber = document.getElementById("pin-number").value;
-    console.log(Amount, pinNumber);
+    const currentAmount = document.getElementById("current-amount").innerText;
+    console.log(currentAmount);
+
+    if(pinNumber === "12490"){
+        const amountInput = parseFloat(Amount);
+        const CurrnetAmount = parseFloat(currentAmount);
+        const updateAmount = amountInput + CurrnetAmount;
+        const TotalAmount = document.getElementById("current-amount").innerText = updateAmount;
+        console.log(updateAmount);
+    }else{
+        console.log("wrong pin number");
+    }
 })
